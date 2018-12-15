22642eed4f96af94f62102931919c2f17b30cc33
jsa-aerial <jsa.aerial@gmail.com>
2018-12-15 15:07:50 -0500

    fix some issues, mostly caused by poor python generator behavior: silently fails on certain errors

--- src/example.py
+++ src/example.py
@@ -1,29 +1,37 @@
 import time
-import client as cli
-from client import msg, open, close, error, bpwait, bpresume, sent, stop, rmv
 import gochans as gc
+import client as cli
+from client import close, error, bpwait, bpresume, sent, stop, rmv
 
 udb = {}
 
 def get_udb (keys):
-    cli.get_db(udb, keys)
+    return cli.get_db(udb, keys)
 
 def update_udb (keys, val):
     cli.update_db(udb, keys, val)
 
 
-dsdict = lambda dict, *keys: (dict[key] for key in keys)
+dsdict = lambda dict, *keys: list((dict[key] for key in keys))
 
 def dispatcher (ch, op, payload):
-    if op == msg or op == 'msg':
+    print("DISPATCH:", op, payload)
+
+    if op == cli.msg or op == 'msg':
         ws, data = dsdict(payload, 'ws', 'data')
         print('CLIENT :msg/payload = ', payload)
         update_udb([ws, 'lastrcv'], data)
         update_udb([ws, 'rcvcnt'], get_udb([ws, 'rcvcnt'])+1)
-    elif op == open:
+    elif op == cli.sent:
+        ws, msg = dsdict(payload, 'ws', cli.msg)
+        print("CLIENT, Sent msg ", msg)
+        update_udb([ws, 'lastsnt'], msg)
+        update_udb([ws, 'sntcnt'], get_udb([ws, 'sntcnt'])+1)
+    elif op == cli.open:
         ws = payload
         print("CLIENT :open/ws = ", ws)
         update_udb([ws], {'chan': ch, 'rcvcnt': 0, 'sntcnt': 0, 'errcnt': 0})
+        update_udb(['com'], [ch, ws])
     elif op == close:
         ws, code, reason = dsdict(payload, 'ws', 'code', 'reason')
         print("CLIENT RMTclose/payload = ", payload)
@@ -41,11 +49,6 @@ def dispatcher (ch, op, payload):
         cli.send_msg(ws, msg, encode=encode)
     elif op == bpresume:
         print("CLIENT, BP Resume ", payload)
-    elif op == sent:
-        ws, msg = dsdict(payload, 'ws', 'msg')
-        print("CLIENT, Sent msg ", msg)
-        update_udb([ws 'lastsnt'], msg)
-        update_udb([ws, 'sntcnt'], get_udb([ws, 'sntcnt'])+1)
     elif op == stop:
         ws, cause = dsdict(payload, 'ws', 'cause')
         print("CLIENT, Stopping reads... Cause ", cause)
@@ -55,6 +58,7 @@ def dispatcher (ch, op, payload):
         print("CLIENT :WTF/op = ", op, " payload = ", payload)
 
 
+# 'ws://localhost:8765/ws'
 def startit (url):
     ch = cli.open_connection(url)
     cli.gorun(ch, dispatcher)
